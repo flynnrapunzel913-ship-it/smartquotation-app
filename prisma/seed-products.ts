d414f118f253da6f687d3575cebf30b1f6796c5e
sanjanashetti13 <sanjanarshetti1306@gmail.com>
2026-06-17 11:19:59 +0530

    update6

--- prisma/seed-products.ts
+++ prisma/seed-products.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma-client";
+import { PrismaClient } from "../generated-client";
 import { MR_PRODUCT_DEFINITIONS } from "../lib/templates/mr-product-definitions";
 
 const prisma = new PrismaClient();
